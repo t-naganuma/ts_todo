18ec721fde954330df2a0eb27fba99ef5c0835c7
Takuya Naganuma <takuya.naganuma@gmail.com>
2021-12-07 11:35:28 +0900

    見た目調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState();
+
+  const addTodos = () => {
+    
+  }
+
   return (
     <div className="App">
       <h1>Todo</h1>
-      <select name="conmpleteOrIncomplete" className="selectBox">
-        <option value=""></option>
-        <option value="incomplete">未完了</option>
-        <option value="complete">完了</option>
-      </select>
       <form className="form">
         <input type="text" placeholder="todo" />
-        <button type="submit">todo追加</button>
+        <button type="submit" onClick={addTodos}>todo追加</button>
       </form>
       <section className="incompleteArea">
         <h2>未完了</h2>
         <ul className="lists">
-          <li>aaa</li>
+          <li>
+            <span>aaa</span>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
         </ul>
       </section>
       <section className="completeArea">
         <h2>完了</h2>
         <ul className="lists">
-          <li></li>
+          <li>
+            <span>aaa</span>
+            <button>戻す</button>
+            <button>削除</button>
+          </li>
         </ul>
       </section>
     </div>
